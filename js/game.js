6acd95507f03eb131df1d47367c9f84a9b7364b2
elif <elifnurilgaz@hotmail.com>
2025-02-06 21:27:44 +0300

    count down

--- js/game.js
+++ js/game.js
@@ -3,11 +3,47 @@ import { Obstacle } from "./obstacle.js";
 import { checkCollision } from "./collision.js";
 import { Score } from "./score.js";
 
+
 const player = new Player(document.getElementById("player"));
 const obstacle = new Obstacle(document.getElementById("obstacle"));
 const score = new Score(document.getElementById("score"));
 
 // const lifes
+const game = function () {
+  let startGame = confirm(
+    "Hello There! Are you redy to play?"
+  );
+  if (startGame){
+    let userName = userInput("Enter your Name", ""); 
+    return count;
+  }
+};
+const userInput = function (text, placeholder = "") {
+  let input = prompt(text, placeholder);
+  if (input == null) {
+    console.error("write your name");
+    return userInput();
+    }
+  else if(input == Number ) {
+    console.error("write your name")
+    return userInput();
+  }
+  else return input;
+};
+let count = 3; 
+const timerElement = document.getElementById("timer");
+
+const interval = setInterval(function() {
+  timerElement.innerHTML = count;
+
+  if (count <= 0) {
+      clearInterval(interval); 
+      timerElement.innerHTML = "Liftoff!"; 
+  } else {
+      count--; 
+  }
+  }, 1000); 
+
 
 console.log("start game");
 document.addEventListener("keydown", (event) => {
@@ -26,3 +62,5 @@ setInterval(() => {
     score.update();
   }
 }, 100);
+
+game();
