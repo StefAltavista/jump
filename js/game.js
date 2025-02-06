6c304fa94a0a7c107a9014d5f9b280be55efd77f
Stef Altavista <stefanomsc@hotmail.it>
2025-02-06 23:58:32 +0100

    user class, input  and display implemented

--- js/game.js
+++ js/game.js
@@ -1,16 +1,19 @@
 import { Sounds } from "./sounds.js";
 import { startGame } from "./start.js";
+import { User } from "./user.js";
 
-const muteButton = document.getElementById("soundMute");
-const startButton = document.getElementById("start");
 const welcomeModal = document.getElementById("welcome");
+const startButton = document.getElementById("start");
 const userDataModal = document.getElementById("userData");
 const submitDataButton = document.getElementById("submitData");
+const userDataInput = document.querySelector(".userDataInput");
+const displayUserName = document.getElementById("displayUserName");
+console.log(displayUserName);
+const muteButton = document.getElementById("soundMute");
 const sounds = new Sounds(muteButton);
 
 muteButton.addEventListener("click", () => {
   sounds.toggleMute();
-  mute = !mute;
 });
 
 startButton.addEventListener("click", () => {
@@ -19,6 +22,22 @@ startButton.addEventListener("click", () => {
 });
 
 submitDataButton.addEventListener("click", () => {
+  let name = userDataInput.value;
+  if (!name) {
+    console.log("error: Name is empty");
+    return;
+  }
+  name = name.trim();
+
+  if (!/^[a-zA-Z0-9\s]+$/.test(name) || name.length < 3 || name.length > 20) {
+    console.log("error: Invalid name");
+    return;
+  }
+
+  const uid = `id-${Date.now()}-${Math.floor(Math.random() * 10000)}`;
+  const user = new User(uid, name);
+  localStorage.setItem(user.uid, JSON.stringify(user));
+  displayUserName.innerText = user.name;
   userDataModal.classList.add("hide");
   startGame(sounds);
 });
---
+++ js/user.js
@@ -0,0 +1,7 @@
+export class User {
+  constructor(uid, name) {
+    this.uid = uid;
+    this.name = name;
+    this.record = 0;
+  }
+}
