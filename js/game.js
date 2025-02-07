a5feef84102078506be6cb9c3fa18d34f5795703
Stef Altavista <stefanomsc@hotmail.it>
2025-02-07 15:21:40 +0100

    modal refactoring

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 import { Sounds } from "./sounds.js";
 import { startGame } from "./start.js";
 import { current, signIn } from "./user.js";
+import { createModal } from "./modals.js";
 
 const welcomeModal = document.getElementById("welcome");
 const enterGame = document.getElementById("enterGame");
@@ -10,6 +11,22 @@ const muteButton = document.getElementById("soundMute");
 const sounds = new Sounds(muteButton);
 
 let user = current();
+
+// const gameOverModal = createModal(
+//   `<div class="gameOver noBackground">
+//   <h2 class="gameOver__title">GAME OVER</h2>
+//   <div class="gameOver__close">Press any key...</div>
+// </div>`,
+//   false
+// );
+
+// const startModal = createModal(
+//   `<div id="welcome" class="modal">
+//       <h2>Welcome to Jump!</h2>
+//       <button id="enterGame">Enter Game</button>
+//     </div>`,
+//   true
+// );
 
 if (user) {
   displayUserName.innerHTML = `<p>${user.name}</p>`;
--- js/modals.js
+++ js/modals.js
@@ -1,39 +1,29 @@
-// Here we create a function for each modal we want to show (start/game over/...) and use renderModal function to show it with attribute as model function.
+const createModal = function (htmlString, actionRequired) {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  const modalMain = document.createElement("div");
+  modalMain.classList.add("modal__main");
+  modalMain.innerHTML = htmlString;
 
-export class Modal {
-  constructor(modalElement) {
-    this.modalElement = modalElement;
-    // this.modalElement.style.display = 'none';
+  modal.append(modalMain);
+  document.body.append(modal);
+
+  if (actionRequired) {
+    modalMain.classList.add("actionModal");
+    modalMain.addEventListener("click", (e) => {
+      e.stopImmediatePropagation();
+    });
   }
 
-  createModalGameOver = ()=>`
-    <div class="gameOver">
-          <h2 class="gameOver__title">GAME OVER</h2>
-          <button class="gameOver__close">Press any key...</button>
-    </div>
-`
-  
-  createModalStart = () => ``
-  
-  countdownModal = () => ``
+  modal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.remove();
+  });
+  document.addEventListener("keydown", (event) => {
+    if (event.key) modal.remove();
+  });
 
+  return modal;
+};
 
-  renderModal (modalFunction){
-    const modal = document.createElement('div');
-    modal.classList.add('modal');
-    const modalMain = document.createElement('div');
-    modalMain.classList.add('modal__main');
-    modalMain.innerHTML=modalFunction();
-    modal.append(modalMain);
-    document.body.append(modal);
-
-    modal.addEventListener("click", ()=>{modal.remove()});
-    document.addEventListener("keydown", (event) => {if (event.key) modal.remove()});
-  };
-
-
-    
-  
-
-  
-}
+export { createModal };
